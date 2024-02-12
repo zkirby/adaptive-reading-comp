d2bf7c862cebae84ebd542f5f54c7ea35b30d794
zkirby <zkirby16@gmail.com>
2024-02-12 16:07:08 -0500

    feat: add skeleton outline for current prompt and questions

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import Questions from "./components/Questions";
+
+/**
+ * @abstract Demo application for the Questions component.
+ */
 function App() {
-  return (<div>hello world</div>);
+  return (<div>
+
+    <div>
+      Current Prompt (on generation [x])
+    </div>
+
+    <Questions />
+  </div>);
 }
 
 export default App;
