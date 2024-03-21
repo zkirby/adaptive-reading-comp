bb76d41bb3020c87611b60eb63fce4a38774b07b
zkirby <zkirby16@gmail.com>
2024-03-20 21:03:37 -0400

    feat: finish the rankings

--- rank.ts
+++ rank.ts
@@ -30,7 +30,21 @@ const saveData = (data: Data) => {
   fs.writeFileSync(DATA_FILE, JSON.stringify(data, null, 2));
 };
 
-const badIds = ["10", "11", "14", "15", "16", "17"];
+const badIds = [
+  "01",
+  "02",
+  "05",
+  "06",
+  "07",
+  "08",
+  "10",
+  "11",
+  "13",
+  "14",
+  "15",
+  "16",
+  "17",
+];
 
 const compareTranslations = async (paragraph: Paragraph) => {
   const translations = [
@@ -77,6 +91,14 @@ const compareTranslations = async (paragraph: Paragraph) => {
   }
 
   const sortedRankings = Object.entries(rankings).sort((a, b) => b[1] - a[1]);
+
+  // Add back all translations that were filtered out
+  for (const translation of translations) {
+    if (!sortedRankings.some(([id]) => id === translation.id)) {
+      sortedRankings.push([translation.id, 100]);
+    }
+  }
+
   return sortedRankings.map(([id], index) => ({ id, rank: index + 1 }));
 };
 
